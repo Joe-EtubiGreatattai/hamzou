b1da1ab16b103166774772a0821adf543251507b
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-10-29 13:02:54 +0100

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -11,8 +11,16 @@ $active=$_POST['active'];
 $date=date("Y/m/d");
 $time=date("h:i:sa");  
 
+	$file="imagefile";
+$pic="images/broken.png";		
+$medianame1=basename( $_FILES["$file"]["name"]);
+$mediatemp1=$_FILES["$file"]['tmp_name'];
+  $uploaddir="images/";
+ $uploaddest1=$uploaddir.$medianame1;
+ if($uploaddest1=="images/"){$uploaddest1=$pic;}
+ move_uploaded_file($mediatemp1,$uploaddest1); 
  
-$in = "INSERT INTO product VALUES ('', '$name', '$dosage', '$active', '$date', '$time', '$img')";
+$in = "INSERT INTO product VALUES ('', '$name', '$dosage', '$active', '$date', '$time', '$uploaddest1')";
 
 if ($conn->query($in) === TRUE) {
     
