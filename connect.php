1681fb3142fcaa256f837d678a4d88fb501592fb
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-10-29 12:31:01 +0100

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,13 @@
+
+
+<?php
+$DB_NAME='hainiger_admin';
+$DB_USER='hainiger_admin';
+$DB_PASSWORD='elf!q*Gau2mJ';
+$DB_HOST='localhost:3306';
+$conn= mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME");
+If ($conn){
+}
+
+
+?>
